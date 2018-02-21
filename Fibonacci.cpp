7d859e925be74f51a137980379aca08ffa4de9be
Avedis777 <avedis777@me.com>
2018-02-20 19:28:08 -0500

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+unsigned int fibonacci (unsigned int n){
+	int a = 1;
+	if (n == 0)
+		return n;
+	else if (n == 1)
+		return n;
+	else {
+		int x = 0;
+		int y = 1;
+		int z;
+		while (n-- > 1){
+			z = x + y;
+			x = y;
+			y = z;
+		}
+		return y;
+	}
+}
